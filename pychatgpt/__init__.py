dad0a96b608963e1d2590386ffb101b3561e544a
Just1z <just1ncs@yandex.ru>
2023-03-02 23:58:17 +0500

    Delete old package

--- pychatgpt/__init__.py
+++
@@ -1,2 +0,0 @@
-""" PyChatGPT is an open-source unofficial asynchronous framework for ChatGPT API written in Python 3.11 using asyncio and aiohttp """
-from .chatgpt import ChatGPT
--- pychatgpt/types/responses.py
+++
@@ -1,4 +0,0 @@
-class ChatCompletion:
-    def __init__(self, **kwargs) -> None:
-        for key, item in kwargs.items():
-            setattr(self, key, item)
